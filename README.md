53aec297f5d2225df66e1c35a3ab08e069195aa6
William Doran <williedoran@gmail.com>
2018-02-12 00:43:27 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a simple app to determine whether or not a paint shop can successfully satisfy users paint requirements.
+To run it on some sample input:
+sbt run /multi-test.txt
--- src/main/scala/paintshop/InputParser.scala
+++ src/main/scala/paintshop/InputParser.scala
@@ -1,33 +1,21 @@
 package paintshop
 
 object InputParser {
-  /*
-      val singleCaseInputLines =
-      """
-        |5
-        |3
-        |1 1 1
-        |2 1 0 2 0
-        |1 5 0
-      """.stripMargin.split("\n").toList
-   */
 
 
   def parse(singleCaseInputLines: Seq[String]) : (Int, Seq[Customer]) = {
-    //validate string
     val numColours :: numCustomers :: customers = singleCaseInputLines
     require(numCustomers.toInt == customers.size, s"${customers.size} number of customers should be ${numCustomers}")
     (numColours.toInt, customers.map(extractCustomerRequirements))
   }
 
   def extractCustomerRequirements(customer: String) : Customer = {
-    //assert constraints that only one matte and that num colors etc are all match
     val numPaints :: paintPairString = customer.split(" ").toList
     require(numPaints.toInt == paintPairString.size / 2, "expected number of customer paints is inconsistent with actual")
-    convertPaintStringToCustomerList(paintPairString)
+    convertPaintStringToCustomerRequirement(paintPairString)
   }
 
-  def convertPaintStringToCustomerList(paints: Seq[String]) : Customer ={
+  def convertPaintStringToCustomerRequirement(paints: Seq[String]) : Customer ={
     val (mattes, glosses) = paints
         .grouped(2)
         .collect{
